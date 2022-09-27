565ee8f7b96f6f36e78433bf8761c190f46a36c8
JovialBadger <68871905+JovialBadger@users.noreply.github.com>
2022-09-27 19:31:25 +0100

    colour change cnd trial

--- javascript/default.js
+++ javascript/default.js
@@ -5,10 +5,6 @@ layout: blank
 //{{ site.urllive }}
 
 var linkProjectsDynamic = [ {% assign sorted_pages = site.pages | sort:"url" %}{% for p in sorted_pages %}{%- if p.title -%}{Name:'{{ p.title }}',URL:'{{ p.url }}'},{%- endif -%}{% endfor %} ];
-var linkBase = "https://jovialbadger.co.uk"
-var linkReddit = "https://www.reddit.com/u/jovialbadger";
-var linkBuyMeACoffee = "https://www.buymeacoffee.com/JovialBadger";
-var linkGithub = "https://github.com/JovialBadger/jovialbadger.github.io";
 
 var testpostsJSON = "{% assign sorted_posts = site.posts | sort:"url" %}{% for x in sorted_posts %}{%- if x.title -%}{{ x.url }}:'{{ x.title }}',{%- endif -%}{% endfor %}";
 
